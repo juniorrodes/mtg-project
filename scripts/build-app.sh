d008b39aa039aebd4a1cf02f006ea13439aaf4ca
José Luis Rodes Maciel Junior <31461584+juniorrodes@users.noreply.github.com>
2024-07-12 00:33:08 -0300

    Adding Tags to docker image (#1)
    * Something
    
    * Adding Tag to images

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -e
+
+TAG = $(git describe --tags &(git rev-list --tags --max-count=1))
+TAG = $(echo $TAG | sed 's/^v//') >> "$GITHUB_ENV"
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build -t juniorrodes/mtg .
+docker build -t juniorrodes/mtg:$TAG .
 
-docker push juniorrodes/mtg
+docker push juniorrodes/mtg:$TAG
