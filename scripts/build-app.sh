406b23a8d3bad75039dfdf2d10a202b2f323e955
juniorrodes <juniorrodes@gmail.com>
2024-07-12 00:29:41 -0300

    Adding Tag to images

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -5,8 +5,6 @@ TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 TAG=$(echo $TAG | sed 's/^v//') >> "$GITHUB_ENV"
 echo "TAG=$TAG" >> "$GITHUB_ENV"
 
-echo $TAG
-
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -t juniorrodes/mtg:$TAG .
 
