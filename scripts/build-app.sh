dddc87213f4d6700d71883de32ecc2f8360e8cfe
juniorrodes <juniorrodes@gmail.com>
2024-07-24 01:32:43 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAA

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -4,6 +4,8 @@ set -e
 TAG=$(git describe --tags &(git rev-list --tags --max-count=1))
 TAG=$(echo $TAG | sed 's/^v//') >> "$GITHUB_ENV"
 
+echo $TAG
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -t juniorrodes/mtg:$TAG .
 
